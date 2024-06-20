ad37d3d642c6532ab73b85f50dc4766ab9b3f199
CodeWithZain10 <zainzahid10116@gmail.com>
2024-06-21 03:05:30 +0500

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-  const express = require("express");
-  const router = express.Router();
-  const blogModel = require("../models/blog");
+const express = require("express");
+const router = express.Router();
+const blogModel = require("../models/blog");
 
-  router.get("/", async (req, res) => {
-    try {
-      let blogs = await blogModel.find().populate("user");
-      res.render("index", { blogs });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Server Error");
-    }
-  });
+router.get("/", async (req, res) => {
+  try {
+    let blogs = await blogModel.find().populate("user");
+    res.render("index", { blogs });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
 
-  module.exports = router;
+module.exports = router;
