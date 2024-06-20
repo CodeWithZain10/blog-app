c444979cf8bd94b3b1a10d6818fa02e0c17a7b4e
CodeWithZain10 <zainzahid10116@gmail.com>
2024-06-21 02:57:40 +0500

    vercel config

--- config/mongoose-config.js
+++ config/mongoose-config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 mongoose
   .connect(`${process.env.MONGODB_URI}`)
   .then(() => {
-    console.log("connected");
+    console.log("connected"); // connected
   })
   .catch((err) => {
     console.log(err);
